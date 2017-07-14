09c84b19824fb90438d8bd7fcbf0d591b18ebc10
Josh DeFriez <jdefriez@gmail.com>
2017-07-14 15:56:15 -0500

    Update quoteMachine.js

--- quoteMachine.js
+++ quoteMachine.js
@@ -1,4 +1,5 @@
 //This is the function to generate random quotes from the favoriteQuotes.json object, "favoriteQuotes"
 
 $("#getQuote").on("click", function {
+                  $getJson
                   });
