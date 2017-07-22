1002566a535b08390006ace0cdc9224ab9926f57
Josh DeFriez <jdefriez@gmail.com>
2017-07-21 19:24:26 -0500

    Update quoteMachine.js

--- quoteMachine.js
+++ quoteMachine.js
@@ -1,6 +1,11 @@
 //This is the function to generate random quotes from the favoriteQuotes.json object, "favoriteQuotes"
 
 $("#getQuote").on("click", function {
-                  $getJSON("favoriteQuotes.json", function(json) {
+                  $getJSON("https://github.com/joshdefriez/randomQuoteMachine/blob/master/favoriteQuotes.json", function(favoriteQuotes) {
+  var randomAuthor = Math.floor(Math.random() * favoriteQuotes.length) + 0);
+  var randomQuote = Math.floor(Math.random() * favoriteQuotes[randomAuthor].length) + 0).toString();
+  var Author = favoriteQuotes[randomAuthor].key 
+  
+  documnet.write( );
 });
                   });
